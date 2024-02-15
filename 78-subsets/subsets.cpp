c95ad251e9805cffd47884661dd8d96ae24acd95
Prabhakar Kulkarni <129973150+PrabhakarKulkarni7@users.noreply.github.com>
2024-02-15 22:55:01 +0530

    Time: 2 ms (57.32%) | Memory: 8.4 MB (51.71%) - LeetSync

---
+++ 78-subsets/subsets.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:  
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>>ans;
+        int num = pow(2, nums.size())-1; 
+
+        for(int i=0; i<=num; i++){   
+            vector<int>st;
+            int index=0, result = num & i;   //Bit Masking 
+ 
+            while(index < nums.size() && result){
+                if((result & 1) == 1)st.push_back(nums[index]); 
+                result = result >> 1;
+                index++;
+            }
+            ans.push_back(st); 
+        } 
+        return ans;
+    }
+};
