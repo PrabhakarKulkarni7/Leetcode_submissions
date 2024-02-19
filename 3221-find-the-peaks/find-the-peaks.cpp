1bbd0f671aea18cfa359ba461e7a725d9df5e454
Prabhakar Kulkarni <129973150+PrabhakarKulkarni7@users.noreply.github.com>
2024-02-19 23:09:11 +0530

    Time: 11 ms (22.94%) | Memory: 23.7 MB (15.56%) - LeetSync

--- 3221-find-the-peaks/find-the-peaks.cpp
+++ 3221-find-the-peaks/find-the-peaks.cpp
@@ -3,7 +3,7 @@ public:
     vector<int> findPeaks(vector<int>& mountain) {
         int n = mountain.size();
         vector<int>temp;
-        for(int i=1;i<n-1;i++)
+        for(int i=1;i<(n-1);i++)
         {
             if(mountain[i]>mountain[i + 1] && mountain[i]>mountain[i - 1])
             {
