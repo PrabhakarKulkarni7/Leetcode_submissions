20ad62579c3c2df0846b8c4e50abb4142aad20e0
Prabhakar Kulkarni <129973150+PrabhakarKulkarni7@users.noreply.github.com>
2024-02-21 13:48:16 +0530

    Time: 3 ms (82.74%) | Memory: 16.5 MB (15.82%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
@@ -1,30 +1,42 @@
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
-        int n = nums.size();
+        int n = nums.size(); // Size of the array
         int low = 0, high = n - 1;
+
         while (low <= high) {
             int mid = (low + high) / 2;
+        // If mid points to the target
             if (nums[mid] == target)
                 return true;
+        // Edge case : if nums[low]==nums[mid]==nums[high]
             if (nums[low] == nums[mid] && nums[mid] == nums[high]) {
                 low = low + 1;
                 high = high - 1;
                 continue;
             }
-            if (nums[low] <= nums[mid]) {
+        // If left part is sorted
+            if (nums[low] <= nums[mid])
+            {
                 if (nums[low] <= target && target <= nums[mid])
+                // Element exists
                     high = mid - 1;
                 else
+                // does not exists
                     low = mid + 1;
-            } else {
-                if (nums[mid] <= target && target <= nums[high]) {
+            } 
+        // If right part is sorted
+            else {
+                if (nums[mid] <= target && target <= nums[high]) 
+                // Element exits
                     low = mid + 1;
-                } else {
+                 else 
+                // does not exist
                     high = mid - 1;
                 }
-            }
         }
         return false;
     }
 };
+
+// If helped please upvote :)
