fd2bf1f22072e568bfb2961aa0155c39805c2769
Prabhakar Kulkarni <129973150+PrabhakarKulkarni7@users.noreply.github.com>
2024-02-24 01:15:52 +0530

    Time: 5 ms (25.45%) | Memory: 10.1 MB (10.01%) - LeetSync

--- 1848-sum-of-unique-elements/sum-of-unique-elements.cpp
+++ 1848-sum-of-unique-elements/sum-of-unique-elements.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
     int sumOfUnique(vector<int>& nums) {
-        map<int,int>mp;
-        for(int i=0;i<nums.size();i++)
+        int n = nums.size(); // size of the vector
+         map<int,int>mp;
+        for(int i=0;i<n;i++)
         {
-            mp[nums[i]]++;
+            // Transfering the array elements to map
+            mp[nums[i]]++;      
         }
 
         int sum = 0;
+        // Check the count of elements, if the count = 1 then it means the ele is unique. Therefore add the ele to sum.
         for(auto it: mp){
             if(it.second==1)
             {
