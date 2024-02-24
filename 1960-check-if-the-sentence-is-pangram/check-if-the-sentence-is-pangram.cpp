2e04bebb20430f45a0f04e9a542610fe3518343c
Prabhakar Kulkarni <129973150+PrabhakarKulkarni7@users.noreply.github.com>
2024-02-24 06:31:43 +0530

    Time: 5 ms (22.00%) | Memory: 7.8 MB (22.15%) - LeetSync

--- 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.cpp
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.cpp
@@ -4,9 +4,11 @@ public:
         set<char>st;
         for(int i=0;i<sentence.size();i++)
         {
+            // Inserting the char into set 
             st.insert(sentence[i]);
         }
 
+        // Checking for size(As Set stores unique elements, if the size = 26 it indicates all the characters from input string are unique, thus follows the anagram property)
         if(st.size()==26){
             return true;
         }
@@ -14,3 +16,4 @@ public:
           
     }
 };
+// Any approach in O(root n) would be appreciable :). Do let me know. Thank you :)
