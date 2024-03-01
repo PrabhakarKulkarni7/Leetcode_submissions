ee93cd02e05b1d1354dfc70529c14be70526454e
Prabhakar Kulkarni <129973150+PrabhakarKulkarni7@users.noreply.github.com>
2024-03-01 22:11:49 +0530

    Time: 4 ms (61.13%) | Memory: 10.7 MB (8.86%) - LeetSync

--- 3055-maximum-odd-binary-number/maximum-odd-binary-number.cpp
+++ 3055-maximum-odd-binary-number/maximum-odd-binary-number.cpp
@@ -1,11 +1,16 @@
 class Solution {
 public:
     string maximumOddBinaryNumber(string s) {
+        // Sort the string in reverse order
         sort(s.rbegin(),s.rend());
+
+        // If the first character is '1', remove it and append '1' to the end
         if(s[0]=='1'){
-            s.erase(0,1);
-            s += '1';
+          s.erase(0,1);  // Erase the first character '1'
+          s +='1';       // Append '1' to the end
         }
+
+        // Return the modified string
         return s;
     }
 };
