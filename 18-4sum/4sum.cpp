85722bff0b9bc82241eb88a566daa06eafc7634d
Prabhakar Kulkarni <129973150+PrabhakarKulkarni7@users.noreply.github.com>
2024-03-02 23:11:05 +0530

    Time: 26 ms (69.13%) | Memory: 16 MB (42.59%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -1,40 +1,65 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        // Get the size of the input vector
         int n = nums.size();
+        
+        // Sort the input vector to simplify the search process
         sort(nums.begin(), nums.end());
+        
+        // Vector to store the resulting quadruplets
         vector<vector<int>> temp;
+        
+        // Loop through each element in the sorted vector
         for (int i = 0; i < n; i++) {
+            // Skip duplicates of the first element to avoid duplicate quadruplets
             if (i > 0 && nums[i] == nums[i - 1])
                 continue;
+            
+            // Loop through each element after the first element
             for (int j = i + 1; j < n; j++) {
+                // Skip duplicates of the second element to avoid duplicate quadruplets
                 if (j > i + 1 && nums[j] == nums[j - 1])
                     continue;
+                
+                // Pointers for the third and fourth elements of the quadruplet
                 int k = j + 1;
                 int l = n - 1;
+                
+                // While the third pointer is less than the fourth pointer
                 while (k < l) {
-                    long long sum = nums[i];
-                    sum = sum + nums[j];
-                    sum = sum + nums[k];
-                    sum = sum + nums[l];
+                    // Calculate the sum of the current quadruplet
+                    //long long sum = nums[i] + nums[j] + nums[k] + nums[l];
+                    long long sum = static_cast<long long>(nums[i]) + nums[j] + nums[k] + nums[l];
 
+                    
+                    // If the sum equals the target, add the quadruplet to the result vector
                     if (sum == target) {
                         vector<int> ans = {nums[i], nums[j], nums[k], nums[l]};
                         temp.push_back(ans);
+                        
+                        // Move the third pointer to the right and the fourth pointer to the left
                         k++;
                         l--;
+                        
+                        // Skip duplicates of the third and fourth elements
                         while (k < l && nums[k] == nums[k - 1])
                             k++;
                         while (k < l && nums[l] == nums[l + 1])
                             l--;
-                    } else if (sum < target) {
+                    } 
+                    // If the sum is less than the target, move the third pointer to the right
+                    else if (sum < target) {
                         k++;
-                    } else {
+                    } 
+                    // If the sum is greater than the target, move the fourth pointer to the left
+                    else {
                         l--;
                     }
                 }
             }
         }
+        // Return the resulting vector of quadruplets
         return temp;
     }
 };
